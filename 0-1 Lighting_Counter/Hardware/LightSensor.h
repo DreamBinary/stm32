da4c5ac1fc24e6b1312a92a7489ae068a86a7fa9
cxq <1647715359@qq.com>
2023-07-10 11:59:46 +0800

    0

---
+++ 0-1 Lighting_Counter/Hardware/LightSensor.h
@@ -0,0 +1,8 @@
+#ifndef __LIGHT_SENSOR_H
+#define __LIGHT_SENSOR_H
+
+void LightSensor_Init(void);
+
+uint8_t Get_Count(void);
+
+#endif
--- 0-1 Lighting_Rate/Hardware/LED.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LED_H
-#define __LED_H
-
-void LED_Init(void);
-void LED_ON(void);
-void LED_OFF(void);
-void LED_Turn(void);
-#endif
